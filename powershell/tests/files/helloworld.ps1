d4838881b6501bb6be2498a49a7e0458311b51b6
Luã Ivo Machado <43272152+limachado@users.noreply.github.com>
2018-10-05 16:10:27 -0300

    Update helloworld.ps1

--- powershell/tests/files/helloworld.ps1
+++ powershell/tests/files/helloworld.ps1
@@ -1,4 +1,4 @@
 # Filename: helloworld.ps1
-Get-WmiObject -Class Win32_Product -ComputerName .
+$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
 #New-Item -Path c:\lua -ItemType directory
 # end of script
