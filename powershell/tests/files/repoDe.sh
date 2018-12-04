3eb737e5377b238ab7379f7501d0925087289119
Luã Ivo Machado <43272152+limachado@users.noreply.github.com>
2018-12-04 14:50:34 -0200

    Update repoDe.sh

--- powershell/tests/files/repoDe.sh
+++ powershell/tests/files/repoDe.sh
@@ -22,5 +22,7 @@ sudo apt-get -f install
  apt-get udpate
 
  ###### sublime ###########
- wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb
- sudo dpkg -i sublime-text_build-3083_amd64.deb
+ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+ sudo apt-get install apt-transport-https
+ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+ sudo apt-get update
