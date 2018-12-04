f671caa22a01f1b489881e236980fa8fc90b4f14
Luã Ivo Machado <43272152+limachado@users.noreply.github.com>
2018-12-04 14:08:38 -0200

    Update configurarRepositorio.sh

--- powershell/tests/files/configurarRepositorio.sh
+++ powershell/tests/files/configurarRepositorio.sh
@@ -1,26 +1,24 @@
 yum update
 
 ###### repo Docker  ######
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-usermod -aG docker your-user
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sh get-docker.sh
+#usermod -aG docker your-user
 
 ###### repo Chrome  ######
-echo '[google-chrome]
-name=google-chrome
-baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
-enabled=1
-gpgcheck=1
-gpgkey=https://dl.google.com/linux/linux_signing_key.pub
-EOF' > /etc/yum.repos.d/google-chrome.repo
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get -f install
+
+###### repo skype  ######
+wget https://repo.skype.com/latest/skypeforlinux-64.deb
+sudo dpkg -i skypeforlinux-64.deb
+sudo apt-get -f install
 
 ###### repo VirtualBox  ######
 
-echo '[virtualbox]
-name=Oracle Linux / RHEL / CentOS-$releasever / $basearch - VirtualBox
-baseurl=http://download.virtualbox.org/virtualbox/rpm/el/$releasever/$basearch
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://www.virtualbox.org/download/oracle_vbox.asc' > /etc/yum.repos.d/virtualbox.repo
-
+ echo "deb http://download.virtualbox.org/virtualbox/debian stretch contrib" >> /etc/apt/sources.list
+ wget -q -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | apt-key add
+ apt-get udpate
+ apt-get install virtualbox-5.1
+ 
